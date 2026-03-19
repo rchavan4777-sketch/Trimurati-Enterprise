11649c43e8dc321d6517b04096ac582b0ef9db0f
student <dhirajchavan355@gmail.com>
2026-03-19 20:42:48 +0530

    fix static export images

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',   // ✅ ADD THIS
-
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "images.unsplash.com" }
